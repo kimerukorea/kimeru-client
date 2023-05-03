0911d8229f4be4d261a1225e3d3e443cce22e3e0
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-05-03 23:50:54 +0900

    문의 성공시 토스트를 보여줍니다

--- src/apps/index/mutations/useMakeInquiryMutation.ts
+++ src/apps/index/mutations/useMakeInquiryMutation.ts
@@ -1,12 +1,25 @@
 import { useMutation } from "@tanstack/react-query";
 import { makeInquiry } from "../apis/makeInquiry";
 import { useInquiryModalStore } from "../stores/inquiryModal.store";
+import { useToast } from "@chakra-ui/react";
 
 export const useMakeInquiryMutation = () => {
+  const toast = useToast();
   const onClose = useInquiryModalStore((state) => state.onClose);
+
   return useMutation(makeInquiry, {
     onSuccess: () => {
       onClose();
+      toast({
+        title: "문의가 접수 되었어요.",
+        description: "빠른 시일내에 보내주신 이메일로 답장 드릴게요.",
+        status: "info",
+        duration: 2000,
+        containerStyle: {
+          width: "100vw",
+        },
+        isClosable: true,
+      });
     },
   });
 };
